abacf4e70e9107987d92f46383d77384dd226e6a
qSided <55927663+qSided@users.noreply.github.com>
2021-06-22 10:11:00 -0500

    Early 2.1.0 - UNFINISHED

---
+++ src/main/java/com/qsided/classesmod/util/SendConfigValues.java
@@ -0,0 +1,19 @@
+package com.qsided.classesmod.util;
+
+import com.qsided.classesmod.config.PlayerClassHandler;
+import java.util.HashMap;
+
+public class SendConfigValues {
+
+    HashMap expboost = PlayerClassHandler.getClasses().get(0).classXpBoosters;
+    String name = PlayerClassHandler.getClasses().get(0).className;
+    String items = PlayerClassHandler.getClasses().get(0).getClassItems().iterator().toString();
+
+    public String encode() {
+        return expboost + "," + name + "," + items;
+    }
+
+    public String[] decode(String data) {
+        return data.split(",");
+    }
+}
